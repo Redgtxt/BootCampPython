fdb58b0d6a2c98bd14bb2fcbd0fa56f18dd6504c
hugo <hp052635@gmail.com>
2025-11-12 17:13:47 +0000

    OPP

---
+++ OOP/main.py
@@ -0,0 +1,21 @@
+# Classes sao escritas em Upercase CarClass exemplo
+
+#from turtle import Turtle,Screen
+from prettytable import PrettyTable
+
+
+#timmy = Turtle();
+
+#print(timmy);
+
+#my_screen = Screen();
+#timmy.color("red");
+#timmy.shape("turtle");
+#timmy.forward(500);
+#timmy.left(120);
+#timmy.forward(500);
+#timmy.left(120);
+#timmy.forward(500);
+
+#my_screen.title("Wow janela com pytohn")
+#my_screen.exitonclick();
